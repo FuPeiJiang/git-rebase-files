2bb7d7eb7251fd13a91377e540fc49610b312828
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2020-12-26 16:39:35 -0500

    dropIndex()

--- extension.js
+++ extension.js
@@ -7,7 +7,8 @@ const child_process = require('child_process')
 const { get } = require('https')
 const path = require('path')
 const shell = require('shelljs')
-
+const p = console.log.bind(console)
+const trash = require('trash')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,6 +17,9 @@ const shell = require('shelljs')
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	p(shell.which('node').toString())
+	shell.config.execPath = shell.which('node').toString()
+
 	// const isWin = process.platform === "win32"
 	// const gitPath = getGitPath()
 	// const gitBashPath = '"' + path.join(path.dirname(path.dirname(gitPath)), "git-bash") + '"'
@@ -28,18 +32,89 @@ function activate(context) {
 			if (!gitRoot)
 				return
 
-				shell.exec('git diff')
+			// p(shell.exec('git diff', { cwd: gitRoot }))
+
+			var output
+			// if (shell.exec('git stash --keep-index --include-untracked').code !== 0) {return}
+
+			// output = shell.exec('git stash --keep-index --include-untracked', { cwd: gitRoot })
+			// if (output.code === 0) { p(output) } else { return }
+			// output = shell.exec('git stash push -m "good stash"', { cwd: gitRoot })
+			// if (output.code === 0) { p(output) } else { return }
+			// output = shell.exec('git stash apply "stash@{1}"', { cwd: gitRoot })
+			// if (output.code === 0) { p(output) } else { return }
+			// output = shell.exec(`git stash show -p stash@{0} --name-only | xargs -I {} git checkout -- "{}"`, { cwd: gitRoot })
+			// if (output.code === 0) { p(output) } else { return }
+
+
+			dropIndex()
+
+
+
+			console.log("done")
+
+			// p("third", 'git stash apply "stash@{1}"')
+			// p(shell.exec('git stash apply "stash@{1}"', { cwd: gitRoot }))
+
+			// p(shell.exec(`comm -1 -2 <(git diff --name-only --cached | sort) <(git diff --name-only stash@{0} HEAD@{1} | sort) | while read line; do git rm -r -f "\${line//[$'\\r']}"; done`, { cwd: gitRoot }))
+
+
+
 			// console.log("gitBashPath", gitBashPath)
 			return
 			var output = child_process.execSync(gitBashPath + " echo foo && echo bar").toString()
 			console.log(output)
 
-
+			async function dropIndex() {
+				// output = shell.exec(`git diff --name-only`, { cwd: gitRoot })
+				output = shell.exec(`git diff --name-only --cached`, { cwd: gitRoot })
+				// output = shell.exec(`git diff --name-only --cached | xargs -d '\\n' sh -c 'for arg do echo "$arg"; cd "$arg"; done' _`, { cwd: gitRoot })
+				// output = shell.exec(`printf "a\\nb" | xargs sh -c 'for arg do echo "\$arg"; cd "\$arg"; done' _`, { cwd: gitRoot })
+	
+				// console.log(`printf "a\\nb" | xargs -d \$'\n' sh -c 'for arg do echo "\$arg"; cd "\$arg"; done' _`, { cwd: gitRoot });
+				const staged = output.slice(0, -1)
+				if (output.code === 0) { p(staged) } else { return }
+				var length, i
+	
+	
+				if (!staged) {
+					throw "nothing staged"
+				}
+	
+				const stagedAr = staged.split('\n')
+				length = stagedAr.length
+	
+				const untracked = shell.exec(`git diff --cached --name-only --diff-filter=A`, { cwd: gitRoot })
+				if (output.code === 0) { p(output) } else { return }
+				const untrackedAr = untracked.slice(0, -1).split('\n')
+	
+				console.log("untrackedAr", untrackedAr)
+				console.log("stagedAr", stagedAr)
+	
+				for (i = 0; i < length; i++) {
+					const relativePath = stagedAr[i]
+					// p("ok", stagedAr[i])
+					output = shell.exec(`git reset HEAD`, { cwd: gitRoot })
+					if (output.code === 0) { p(output) } else { return }
+	
+					if (untrackedAr.includes(relativePath)) {
+						//reeeeeeeeeeeeeeeeeeeeeeeeeee
+	
+						await trash(path.join(gitRoot, relativePath))
+						console.log(path.join(gitRoot, relativePath))
+					} else {
+						console.log(`git checkout -- "${relativePath}"`);
+						output = shell.exec(`git checkout -- "${relativePath}"`, { cwd: gitRoot })
+						if (output.code === 0) { p(output) } else { return }
+					}
+				}
+			}
 		} catch (error) {
 			const strError = error.toString()
 			console.log(strError)
 			vscode.window.showInformationMessage(strError)
 		}
+		
 	}))
 
 	context.subscriptions.push(vscode.commands.registerCommand('git-rebase-files.helloWorld', async function () {
@@ -109,7 +184,9 @@ module.exports = {
 	console.log(`stdout: ${stdout}`);
 	console.error(`stderr: ${stderr}`);
   }); */
-
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 function getGitRoot() {
 	return new Promise(async (resolve) => {
@@ -165,3 +242,9 @@ function getGitRoot() {
 
 
 }
+
+
+// function p(toPrint) {
+	// console.log(toPrint)
+	// vscode.window.showInformationMessage(toPrint)
+// }
