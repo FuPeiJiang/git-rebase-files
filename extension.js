8e898f7840f6e011c603ed1b7613aca81549bb2b
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2020-12-27 11:59:37 -0500

    removed comments

--- extension.js
+++ extension.js
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
 const window = vscode.window
 const fs = require('fs')
@@ -10,20 +8,13 @@ const shell = require('shelljs')
 const p = console.log.bind(console)
 const trash = require('trash')
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// p(shell.which('node').toString())
 	shell.config.execPath = shell.which('node').toString()
 	var savedGitRoot, savedCommitId
 	var notTerminalExist = true, terminal
-	// const isWin = process.platform === "win32"
-	// const gitPath = getGitPath()
-	// const gitBashPath = '"' + path.join(path.dirname(path.dirname(gitPath)), "git-bash") + '"'
 
 	context.subscriptions.push(vscode.commands.registerCommand('git-rebase-files.stash-staged-only', async function () {
 		try {
@@ -33,13 +24,8 @@ function activate(context) {
 
 			const stashMessage = await window.showInputBox({ prompt: "please put a stash message" })
 
-			// p(shell.exec('git diff', { cwd: gitRoot }))
 
 			var output
-			// if (shell.exec('git stash --keep-index --include-untracked').code !== 0) {return}
-
-			// p(shell.exec('git config --global user.email', { cwd: gitRoot }))
-			// return
 
 			output = shell.exec('git stash --keep-index --include-untracked', { cwd: gitRoot })
 			if (output.code === 0) { p(output) } else { return }
@@ -54,21 +40,6 @@ function activate(context) {
 
 			//attention, this is async
 			dropIndex() //we can do this since we stashed our stuff
-
-
-			console.log("done")
-
-			// p("third", 'git stash apply "stash@{1}"')
-			// p(shell.exec('git stash apply "stash@{1}"', { cwd: gitRoot }))
-
-			// p(shell.exec(`comm -1 -2 <(git diff --name-only --cached | sort) <(git diff --name-only stash@{0} HEAD@{1} | sort) | while read line; do git rm -r -f "\${line//[$'\\r']}"; done`, { cwd: gitRoot }))
-
-
-
-			// console.log("gitBashPath", gitBashPath)
-			return
-			var output = child_process.execSync(gitBashPath + " echo foo && echo bar").toString()
-			console.log(output)
 
 			async function dropIndex() {
 				var output
@@ -138,25 +109,11 @@ function activate(context) {
 			console.log((commitId))
 
 			var output
-			// output = shell.exec(`git commit --fixup=${commitId}`, { cwd: gitRoot })
-			// if (output.code === 0) { p(output) } else { return }
-
-			// console.log(`GIT_SEQUENCE_EDITOR=true git rebase --interactive --autosquash "${commitId}^"`)
 
 			shell.env["GIT_SEQUENCE_EDITOR"] = "true"
 			output = shell.exec(`git rebase --interactive --autosquash "${commitId}^"`, { cwd: gitRoot })
-			// output = shell.exec(`GIT_SEQUENCE_EDITOR="true" git rebase --interactive --autosquash "${commitId}^"`, { cwd: gitRoot })
 			if (output.code === 0) { p(output) } else { return }
 
-
-			// var repoName
-			// var lastSlash = gitRoot.lastIndexOf('/')
-			// if (lastSlash != -1) {
-			// repoName = gitRoot.slice(lastSlash + 1)
-			// } else {
-			// vscode.window.showInformationMessage("path is not a dir and has no parent dir")
-			// return
-			// }
 		} catch (error) {
 			const strError = error.toString()
 			console.log(strError)
@@ -190,16 +147,6 @@ function activate(context) {
 			output = shell.exec(`git reset --soft "${commitId}^"`, { cwd: gitRoot })
 			if (output.code === 0) { p(output) } else { return }
 
-
-
-			// var repoName
-			// var lastSlash = gitRoot.lastIndexOf('/')
-			// if (lastSlash != -1) {
-			// repoName = gitRoot.slice(lastSlash + 1)
-			// } else {
-			// vscode.window.showInformationMessage("path is not a dir and has no parent dir")
-			// return
-			// }
 		} catch (error) {
 			const strError = error.toString()
 			console.log(strError)
@@ -214,16 +161,7 @@ function activate(context) {
 			if (!savedCommitId)
 				throw "no savedCommitId from git-rebase-files.edit-past-commit"
 
-
 			var output
-			// output = shell.exec(`git commit --fixup=${commitId}`, { cwd: gitRoot })
-			// if (output.code === 0) { p(output) } else { return }
-
-			// console.log(`GIT_SEQUENCE_EDITOR=true git rebase --interactive --autosquash "${commitId}^"`)
-
-
-
-
 
 			output = shell.exec(`git commit -C "${savedCommitId}"`, { cwd: savedGitRoot })
 			if (output.code === 0) { p(output) } else { return }
@@ -231,14 +169,6 @@ function activate(context) {
 			output = shell.exec(`git rebase --continue`, { cwd: savedGitRoot })
 			if (output.code === 0) { p(output) } else { return }
 
-			// var repoName
-			// var lastSlash = gitRoot.lastIndexOf('/')
-			// if (lastSlash != -1) {
-			// repoName = gitRoot.slice(lastSlash + 1)
-			// } else {
-			// vscode.window.showInformationMessage("path is not a dir and has no parent dir")
-			// return
-			// }
 		} catch (error) {
 			const strError = error.toString()
 			console.log(strError)
@@ -257,17 +187,6 @@ function activate(context) {
 				throw "no commit id"
 
 			var output
-
-			// shell.env["GIT_SEQUENCE_EDITOR"] = 'code -w'
-			// shell.env["GIT_SEQUENCE_EDITOR"] = `code -w -n`
-			// shell.env["EDITOR"] = "'code -w'"
-
-			// console.log(`git rebase --interactive "${commitId}^"`);
-			// output = 
-			// child_process.execSync(`git rebase --interactive "${commitId}^"`, { cwd: gitRoot })
-			// output = shell.exec(`git rebase --interactive "${commitId}^"`, { cwd: gitRoot })
-			// if (output.code === 0) { p(output) } else { return }
-
 
 			if (notTerminalExist) {
 				notTerminalExist = false
